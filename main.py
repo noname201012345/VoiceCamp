799e1fb02faa09b834d8582c0342bc9f82b12099
noname201012345 <58730410+noname201012345@users.noreply.github.com>
2022-09-18 08:45:22 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+import discord
+from os
+import json
+
+bot = commands.Bot("!", self_bot=True) # Defining bot.
+
+guild=755793441287438469
+channel=994552773637062656
+
+@bot.event # Turning the bot online.
+async def on_ready():
+    print("This program has logged in to the account " + f"{bot.user}.\n")
+    voice_channel = discord.utils.get(bot.get_guild(guild).channels, id = channel)
+    await voice_channel.connect()
+        
+bot.run(os.getenv["token"], bot=False)
