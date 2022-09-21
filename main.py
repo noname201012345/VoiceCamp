eb7f3604987cdc3c487a58c50ad6388bc3cef8b1
noname201012345 <58730410+noname201012345@users.noreply.github.com>
2022-09-21 13:17:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 import discord
 import os
 from discord.ext import commands
-import requests
-import asyncio
-import json
-import base64
+from rerun import rerun
 
 client=commands.Bot(command_prefix=':', self_bot=True, help_command=None)
 
 GUILD_ID = 755793441287438469
 CHANNEL_ID = 994552773637062656
-
-rtoken = os.getenv("RTOKEN")
-header = {"Authorization": "Bearer {}".format(rtoken)}
-link="https://api.github.com/repos/noname201012345/VoiceCamp/contents/"
 
 @client.event
 async def on_ready():
@@ -22,16 +15,6 @@ async def on_ready():
     vc = discord.utils.get(client.get_guild(GUILD_ID).channels, id = CHANNEL_ID)
     await vc.connect()
     print(f"Successfully joined {vc.name} ({vc.id})")
-    await asyncio.sleep(100)
-    with open("rerun.json", "r") as f:
-        rerun = json.load(f)
-    if rerun["id"]==1:
-        rerun["id"]=0
-    elif rerun["id"]==0:
-        rerun["id"]=1
-    r = requests.get(link+"rerun.json",headers=header)
-    sh=r.json()["sha"]
-    base64S= base64.b64encode(bytes(json.dumps(rerun), "utf-8"))
-    rjson = {"message":"cf", "content":base64S.decode("utf-8"),"sha":sh}
-    response = requests.put(link+"rerun.json", data=json.dumps(rjson), headers=header)
+
 client.run(os.getenv("TOKEN"))
+rerun()
